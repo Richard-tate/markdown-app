8110d662f3b6060abca099265654eb19f70b818a
Richard-tate <62254930+Richard-tate@users.noreply.github.com>
2020-11-25 14:26:42 +0000

    markdown app

---
+++ markdown/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './DarkApp.css';
+
+import marked from 'marked';
+
+import { sampleText } from './sampleText';
+
+marked.setOptions({
+  breaks: true
+});
+
+class App extends Component {
+  state = {
+    text: sampleText,
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text');
+
+    if (text) {
+      this.setState({ text });
+    } else {
+      this.setState({ text: sampleText });
+    }
+  }
+
+  componentDidUpdate () {
+    const { text } = this.state;
+    localStorage.setItem('text', text);
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
+
+  render () {
+  
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <textarea
+              onChange={this.handleChange}
+              value={this.state.text}
+              className='form-control'
+              rows='35' />
+          </div>
+          <div className='col-sm-6'>
+            <div className='box' dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default App;
